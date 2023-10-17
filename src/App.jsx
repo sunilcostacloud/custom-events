6672487dac168dd78fbe0e035721d31451059e41
sunilcostacloud <sunil@costacloud.com>
2023-10-17 15:54:35 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Section1 from "./components/Section1"
+import Section2 from "./components/Section2"
+import Section3 from "./components/Section3"
+
+const App = () => {
+  return (
+    <div>
+      <Section1 />
+      <hr />
+      <Section2 />
+      <hr />
+      <Section3 />
+    </div>
+  )
+}
+
+export default App
